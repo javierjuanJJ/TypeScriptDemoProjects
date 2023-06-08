7e251dd0eb13b98e46dbf4a167313886d4137b3b
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-06-08 17:49:13 +0200

    Commit 3

--- TypeScriptDemo/class.ts
+++ TypeScriptDemo/class.ts
@@ -1,12 +1,27 @@
 "use strict";
 
-class Employee {
-    private _id:number;
-    private _name:string;
-    private _address: string;
+interface Address {
+    street: string;
+    city: string;
+    state: string;
+    pin: string;
+}
+
+interface IEmployees extends Address {
+    salary: number;
+}
+
+class Employee implements login {
+    private _id: number;
+    private _name: string;
+    private _address: Address;
+
+    static getone(): number {
+        return 50;
+    }
 
 
-    constructor(id: number, name: string, address: string) {
+    constructor(id: number, name: string, address: Address) {
         this._id = id;
         this._name = name;
         this._address = address;
@@ -29,16 +44,63 @@ class Employee {
         this._name = value;
     }
 
-    get address(): string {
+    get address(): Address {
         return this._address;
     }
 
-    set address(value: string) {
+    set address(value: Address) {
         this._address = value;
+    }
+
+    login(): IEmployees {
+        return {
+            street: 'string',
+            city: 'string',
+            state: 'string',
+            pin: 'string',
+            salary: 1,
+        };
     }
 
 
 }
 
-let employee1 = new Employee(1,"2","9");
+let employee1 = new Employee(1, "2", {
+    street: 'string',
+    city: 'string',
+    state: 'string',
+    pin: 'string',
+});
+
+let {id, name, address}: Employee = new Employee(1, "2", {
+    street: 'string',
+    city: 'string',
+    state: 'string',
+    pin: 'string',
+});
+
+console.log(id);
+console.log(name);
+console.log(address);
+
 console.log(employee1);
+
+class Manager extends Employee {
+    constructor(id: number, name: string, address: Address) {
+        super(id, name, address);
+    }
+
+}
+
+let manager1 = new Manager(1, "2", {
+    street: 'string',
+    city: 'string',
+    state: 'string',
+    pin: 'string',
+});
+console.log(manager1);
+console.log(Employee.getone());
+
+export interface login {
+    login(): IEmployees;
+}
---
+++ TypeScriptDemo/interface.ts
@@ -0,0 +1,13 @@
+interface IUser {
+    name:string;
+    age:number;
+    id:number;
+    email:string;
+}
+
+var Users = {
+    name:'string',
+    age:'number',
+    id:1,
+    email:'string',
+}
