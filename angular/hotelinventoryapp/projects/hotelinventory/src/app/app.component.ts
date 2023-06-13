5072349b80d048fe7c29d2b4728c22e4f5db4567
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-06-13 10:57:23 +0200

    Commit 3

---
+++ angular/hotelinventoryapp/projects/hotelinventory/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: '<h1>Hellow World inline</h1> <p>Angular is Awesome</p>',
+  styleUrls: ['./app.component.css'],
+  styles: [`h1 {color: red;}`]
+})
+export class AppComponent {
+  title = 'hotelinventory';
+}
