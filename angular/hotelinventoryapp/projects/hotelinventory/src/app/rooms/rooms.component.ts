af355d63946a43fad55a2836ed98e68f25e3a7a4
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-06-13 10:57:28 +0200

    Commit 3

---
+++ angular/hotelinventoryapp/projects/hotelinventory/src/app/rooms/rooms.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit {
+
+  hotelRoom = 'Hilton Hotel';
+
+  constructor() {
+
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
